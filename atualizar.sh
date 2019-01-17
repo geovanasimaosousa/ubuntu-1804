d13c634bb1fd6862cc87057adc8d35e1cc060d35
geovanasimaosousa <43184826+geovanasimaosousa@users.noreply.github.com>
2019-01-17 16:16:26 -0200

    Create atualizar.sh

---
+++ atualizar.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Script para autalizar o GNU/linux
+
+echo Atualizar as Listas do Apt
+echo
+	sudo apt update
+echo
+
+echo Listas atualizadas
+echo Atualizando os Software
+echo
+	sudo apt -y upgrade
+echo
+echo Software atualizadas
+
+echo Atualizar o kernel
+echo
+	sudo apt -y dist-upgrade
+echo
+echo kernel Atualizado
+
+echo Removendo Software
+echo
+	sudo apt -y autoremove
+echo
+echo Software removendo
+
+echo Limpando o cache
+echo
+	sudo apt -y autoclean
+echo
+echo cache limpo
