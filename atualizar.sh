940a31014f41bbeb311cedec36a417674db9b59e
geovanasimaosousa <43184826+geovanasimaosousa@users.noreply.github.com>
2019-01-17 16:33:50 -0200

    Update atualizar.sh

--- atualizar.sh
+++ atualizar.sh
@@ -7,11 +7,18 @@ echo
 echo
 
 echo Listas atualizadas
+
+sleep 5
+clear
+
 echo Atualizando os Software
 echo
 	sudo apt -y upgrade
 echo
 echo Software atualizadas
+
+sleep 5
+clear
 
 echo Atualizar o kernel
 echo
@@ -20,6 +27,9 @@ echo
 echo kernel Atualizado
 
 echo Removendo Software
+
+sleep 5
+clear
 echo
 	sudo apt -y autoremove
 echo
@@ -30,3 +40,5 @@ echo
 	sudo apt -y autoclean
 echo
 echo cache limpo
+sleep 5
+clear
